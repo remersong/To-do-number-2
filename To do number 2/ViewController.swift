483b23a9a98f17025abc3ea725b4a06a86ebc276
Rob <remersong@gmail.com>
2019-12-04 08:45:30 -0500

    Initial commit

---
+++ To do number 2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  To do number 2
+//
+//  Created by Robert_Glazer on 11/26/19.
+//  Copyright © 2019 Robert_Glazer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.isEditing = true
+        // Do any additional setup after loading the view.
+        
+        
+        
+    }
+
+
+    //create list of items including an initial
+}
+
